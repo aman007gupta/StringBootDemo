c05fb830f1297513b2b415d334338f97e67c0603
aman007gupta <aman9453122969@gmail.com>
2021-01-15 15:40:51 +0530

    [Aman]Added:code to demonstrate dependency injection using Emp and department Bean

--- src/main/java/com/example/helloworlddemo/HelloworlddemoApplication.java
+++ src/main/java/com/example/helloworlddemo/HelloworlddemoApplication.java
@@ -1,14 +1,29 @@
 package com.example.helloworlddemo;
 
+import com.example.helloworlddemo.component.DemoBean;
+import com.example.helloworlddemo.component.EmployeeBean;
+import com.example.helloworlddemo.controller.HelloRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HelloworlddemoApplication {
+    public static final Logger logger = LoggerFactory.getLogger(HelloworlddemoApplication.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        SpringApplication.run(HelloworlddemoApplication.class, args);
+        logger.debug("Hello!");
+        ApplicationContext context = SpringApplication.run(HelloworlddemoApplication.class, args);
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        logger.info("Demo Bean= " + demoBean.toString());
+        System.out.println(context.getBean(HelloRestController.class));
+        logger.debug("\n*** Example Using @Autowire annotation on property");
+        EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+        employeeBean.setEid(100);
+        employeeBean.setEname("Aman");
+        employeeBean.showEmployeeDetails();
     }
 
 }
---
+++ src/main/java/com/example/helloworlddemo/component/DemoBean.java
@@ -0,0 +1,9 @@
+package com.example.helloworlddemo.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBean {
+    public DemoBean() {
+    }
+}
